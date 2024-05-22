667b1b1f01d0db08bde269922a5514e35ff522e2
Pavel <Pavel.Vlasov@nasdanika.org>
2024-05-22 08:59:51 -0400

    Inspector predicate

--- src/main/java/org/nasdanika/launcher/demo/rules/inspectors/ReflectiveInspectorFactory.java
+++ src/main/java/org/nasdanika/launcher/demo/rules/inspectors/ReflectiveInspectorFactory.java
@@ -2,30 +2,28 @@ package org.nasdanika.launcher.demo.rules.inspectors;
 
 import java.util.concurrent.CompletionStage;
 import java.util.function.BiFunction;
+import java.util.function.Predicate;
 
 import org.nasdanika.capability.CapabilityProvider;
 import org.nasdanika.capability.ServiceCapabilityFactory;
 import org.nasdanika.common.ProgressMonitor;
 import org.nasdanika.models.rules.Inspector;
+import org.nasdanika.models.rules.InspectorCapabilityFactory;
 import org.nasdanika.models.rules.reflection.InspectorSet;
 import org.nasdanika.models.rules.reflection.RuleManager;
 
-public class ReflectiveInspectorFactory extends ServiceCapabilityFactory<Object, Inspector<Object>> {
-
-	@Override
-	public boolean isForServiceType(Class<?> type) {
-		return type == Inspector.class;
-	}
+public class ReflectiveInspectorFactory extends InspectorCapabilityFactory<Object> {
 
 	@Override
 	protected CompletionStage<Iterable<CapabilityProvider<Inspector<Object>>>> createService(
 			Class<Inspector<Object>> serviceType, 
-			Object serviceRequirement,
+			Predicate<Inspector<Object>> serviceRequirement,
 			BiFunction<Object, ProgressMonitor, CompletionStage<Iterable<CapabilityProvider<Object>>>> resolver,
 			ProgressMonitor progressMonitor) {
 
 		Inspector<Object> inspector = new InspectorSet(
 			RuleManager.LOADING_RULE_MANAGER, 
+			serviceRequirement,
 			false, 
 			progressMonitor, 
 			new ReflectiveInspectors());
