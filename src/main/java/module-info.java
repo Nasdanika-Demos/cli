4433769d3dca3048d01e6ca33e13a8c82e707921
Pavel Vlasov <Pavel.Vlasov@nasdanika.org>
2025-03-15 11:28:20 -0400

    Refinement of requirements

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -16,13 +16,17 @@ module org.nasdanika.launcher.demo {
 	requires org.nasdanika.launcher;
 	requires org.nasdanika.models.echarts.graph;
 	requires org.nasdanika.models.app.graph;
-	requires java.sql;
-	requires java.xml;
+//	requires java.sql;
+//	requires java.xml;
 	requires java.xml.bind;
 	requires org.nasdanika.models.java.cli;
 	requires reactor.netty.http;
 	requires org.nasdanika.http;
 	requires org.nasdanika.cli;
+	requires org.nasdanika.models.app.cli;
+	requires org.nasdanika.models.rules.cli;
+	requires org.nasdanika.models.architecture;
+	requires org.nasdanika.models.ecore.cli;
 	
 	opens org.nasdanika.launcher.demo to info.picocli;
 	opens org.nasdanika.launcher.demo.java to info.picocli;
